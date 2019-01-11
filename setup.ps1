e600860df904bb0cd216afa766e9910dc55058f8
Taylor Vesely <dreddor@dreddor.net>
2019-01-11 13:36:30 -0800

    setup.ps1: Add setup for ansible to connect to Windows
    Adds a script that will enable WinRM so that Ansible can be used to
    configure the host.
    
    Additionally, separate each step of the setup process into its own
    Powershell function.

--- setup.ps1
+++ setup.ps1
@@ -1,33 +1,58 @@
 # Import the certificate that signed all these scripts
-$cert = Import-Certificate -Filepath "$PSScriptRoot\dreddor_root_cert.cert" `
-  -CertStoreLocation cert:\LocalMachine\Root
+Function ImportSelfSigningCert() {
+    $cert = Import-Certificate -Filepath "$PSScriptRoot\dreddor_root_cert.cert" `
+      -CertStoreLocation cert:\LocalMachine\Root
+}
 
-$write_profile = $TRUE
+Function SetupProfile() {
 
-If(Test-Path $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1) {
-    If(Test-Path $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1.old) {
-        Write-Host "Old powershell profile already exists. Skipping"
-        $write_profile = $FALSE
-    } Else {
-        Write-Host "Found existing powershell profile- Moving aside."
-        mv $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1 `
-           $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1.old
+    $write_profile = $TRUE
+
+    If(Test-Path $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1) {
+        If(Test-Path $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1.old) {
+            Write-Host "Old powershell profile already exists. Skipping"
+            $write_profile = $FALSE
+        } Else {
+            Write-Host "Found existing powershell profile- Moving aside."
+            mv $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1 `
+               $HOME\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1.old
+        }
+    }
+
+    # Link the powershell profile to this powershell profile
+    If($write_profile) {
+        New-Item -Path $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 `
+          -ItemType SymbolicLink `
+          -Value $PSScriptRoot\Microsoft.PowerShell_profile.ps1
     }
 }
 
-# Link the powershell profile to this powershell profile
-If($write_profile) {
-    New-Item -Path $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 `
-      -ItemType SymbolicLink `
-      -Value $PSScriptRoot\Microsoft.PowerShell_profile.ps1
+Function SetupWinRMForAnsible() {
+    $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+    $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+    (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+    powershell.exe -ExecutionPolicy ByPass -File $file
 }
 
+Function SetupWSL() {
+    return
+}
+
+Function Main() {
+    ImportSelfSigningCert
+    SetupProfile
+    SetupWinRMForAnsible
+}
+
+Main
 
 # SIG # Begin signature block
 # MIIFrAYJKoZIhvcNAQcCoIIFnTCCBZkCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUdo8lXwA+7PEB7YjzOiwSmxXo
-# OaigggMyMIIDLjCCAhagAwIBAgIQFlDAr68W6LtJ2eMhRB8hRTANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUOl6/dk7n/VRYHy1KglewR93G
+# hZugggMyMIIDLjCCAhagAwIBAgIQFlDAr68W6LtJ2eMhRB8hRTANBgkqhkiG9w0B
 # AQsFADAvMS0wKwYDVQQDDCREcmVkZG9yIFNlbGYtU2lnbmVkIENvZGUgQ2VydGlm
 # aWNhdGUwHhcNMTkwMTExMTkzNDA1WhcNMjAwMTExMTk1NDA1WjAvMS0wKwYDVQQD
 # DCREcmVkZG9yIFNlbGYtU2lnbmVkIENvZGUgQ2VydGlmaWNhdGUwggEiMA0GCSqG
@@ -48,11 +73,11 @@ If($write_profile) {
 # aWduZWQgQ29kZSBDZXJ0aWZpY2F0ZQIQFlDAr68W6LtJ2eMhRB8hRTAJBgUrDgMC
 # GgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYK
 # KwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG
-# 9w0BCQQxFgQUEICynXmydC1DOqlkw0ES6EFm790wDQYJKoZIhvcNAQEBBQAEggEA
-# wHhHEy2CjkWkXkmrsew8mXVSQle1V13aUei25+7MNXEQzeiGAYBTNi+C7pGwVMT1
-# 857fVSg6vEStEtTqvUzH/+gL7+uGpLJnI25HbyAE+JLP7iV4IoHULHc+gzUGh9tP
-# arzEnx21yT/EGi9JnwPiLyS4Vr3r7nmGlv9Gzu8Q8J7+zM8cskAxhony4UKv7SGV
-# nFJ14T2m3nlbm6WQ+n4qrpKYHCobe3nopKmfb3qXdCvkv0rN1JBRf3apJMO/ylKy
-# 0wawidOtx7E+p1br7BYMWCVwEz3l95jEuvxoiOI6tlninN46Q0qhb7QnM41ArvkK
-# rqQXHK3+OPFm/mGdTB10Fw==
+# 9w0BCQQxFgQUXkBaYRkiZwvQ23V91JSuAUgFzLMwDQYJKoZIhvcNAQEBBQAEggEA
+# XtMoAffbUFOskTdktXHozBBv6u4qHrk+L56/+AHZYR0sYXkZU0KsiDaZ7Bkarcz9
+# aJBTHE5rJPbONCJp9jrIK1ZQ100iwsNlp/OUIaI3vHAznsEAB6E3t/i6IwAgeIq6
+# 8Ahb9fxQkquSW86xCkpv4WmWEV5sa3DMhsIU3mYz6dHQjgccEQNvHqOTWSOBcvkF
+# eayqDvwNeSQHmp7c44+GMh55Sh3Rm8Ww0nNbUGL4/UJjSV4CkmCLDNewp1mXjPa6
+# IPSCXmuaSiad66Z2Z5d+YqbFnjakqGHBY/J/7oaM0xJL3N38PkBSFP0U/hzu9ktl
+# gSwV0sHM3/ytUiCpz2paeQ==
 # SIG # End signature block

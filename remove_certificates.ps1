a1e7013d17cef8ad5d6aa367a5e329e34ec5edac
Taylor Vesely <dreddor@dreddor.net>
2019-04-11 14:46:20 -0700

    remove_certificates: Remove Code Signing Certs

--- remove_certificates.ps1
+++ remove_certificates.ps1
@@ -1,15 +1,49 @@
 Set-StrictMode -Version Latest
 $ErrorActionProference = "Stop"
 
+Function RemoveCodeSigningCertificates {
+    $CertificatePath = "$PSScriptRoot\ansible\certificates\CodeSigningCert.pfx"
+    $CodeSigningCert = Get-PfxCertificate -FilePath $CertificatePath
+    $CodeSigningCertThumbprint = $CodeSigningCert.Thumbprint.ToString()
+
+    if (Test-Path Cert:\LocalMachine\Root\$CodeSigningCertThumbprint) {
+        Write-Host "Removing Code Signing Certificate from 'Root' certificate store..."
+        Remove-Item Cert:\LocalMachine\Root\$CodeSigningCertThumbprint
+    } Else {
+        Write-Host "Code Signing Certificate is not installed in the 'Root' certificate store. Skipping."
+    }
+
+    if (Test-Path Cert:\LocalMachine\My\$CodeSigningCertThumbprint) {
+        Write-Host "Removing Code Signing Certificate from 'My' certificate store..."
+        Remove-Item Cert:\LocalMachine\My\$CodeSigningCertThumbprint
+    } Else {
+        Write-Host "Code Signing Certificate is not installed in the 'My' certificate store. Skipping."
+    }
+
+    if (Test-Path Cert:\LocalMachine\CA\$CodeSigningCertThumbprint) {
+        Write-Host "Removing Code Signing Certificate from 'CA' certificate store..."
+        Remove-Item Cert:\LocalMachine\CA\$CodeSigningCertThumbprint
+    } Else {
+        Write-Host "Code Signing Certificate is not installed in the 'CA' certificate store. Skipping."
+    }
+
+    if (Test-Path Cert:\LocalMachine\TrustedPeople\$CodeSigningCertThumbprint) {
+        Write-Host "Removing Code Signing Certificate from the 'TrustedPeople' certificate store..."
+        Remove-Item Cert:\LocalMachine\TrustedPeople\$CodeSigningCertThumbprint
+    } Else {
+        Write-Host "Code Signing Certificate is not installed in the 'Trusted People' certificate store. Skipping."
+    }
+}
+
 Function RemoveWinRMCertificates {
     $CertificatePath = "$PSScriptRoot\ansible\certificates\WinRMCert.pfx"
     $PrivateWinRMCert = Get-PfxCertificate -FilePath $CertificatePath
     $PrivateThumbprint = $PrivateWinRMCert.Thumbprint.ToString()
 
     # Try to find Private Key in WSMan
-    $WSManClientCert = Get-ChildItem  WSMan:\localhost\ClientCertificate\  | 
-        Where-Object { 
-            $_ | Get-ChildItem | 
+    $WSManClientCert = Get-ChildItem  WSMan:\localhost\ClientCertificate\  |
+        Where-Object {
+            $_ | Get-ChildItem |
                 Where-Object {($_).Name  -eq "Issuer" -and  $_.Value -eq $PrivateThumbprint }
         } 
 
@@ -21,11 +55,25 @@ Function RemoveWinRMCertificates {
         Write-Host "WinRM Client Certificate does not exist in WSMan. Skipping"
     }
 
- 	if (Test-Path Cert:\LocalMachine\Root\$PrivateThumbprint) {
-        Write-Host "Removing WinRM Certificate from the certificate store..." 
-		Remove-Item Cert:\LocalMachine\Root\$PrivateThumbprint
+    if (Test-Path Cert:\LocalMachine\Root\$PrivateThumbprint) {
+        Write-Host "Removing WinRM Certificate from 'Root' certificate store..."
+        Remove-Item Cert:\LocalMachine\Root\$PrivateThumbprint
     } Else {
-        Write-Host "WinRM Private Certificate is not installed in the certificate store. Skipping."
+        Write-Host "WinRM Private Certificate is not installed in the 'Root' certificate store. Skipping."
+    }
+
+    if (Test-Path Cert:\LocalMachine\My\$PrivateThumbprint) {
+        Write-Host "Removing WinRM Certificate from 'My' certificate store..."
+        Remove-Item Cert:\LocalMachine\My\$PrivateThumbprint
+    } Else {
+        Write-Host "WinRM Private Certificate is not installed in the 'My' certificate store. Skipping."
+    }
+
+    if (Test-Path Cert:\LocalMachine\CA\$PrivateThumbprint) {
+        Write-Host "Removing WinRM Certificate from 'CA' certificate store..."
+        Remove-Item Cert:\LocalMachine\CA\$PrivateThumbprint
+    } Else {
+        Write-Host "WinRM Private Certificate is not installed in the 'CA' certificate store. Skipping."
     }
 
     # Import Public Key
@@ -35,16 +83,16 @@ Function RemoveWinRMCertificates {
     #
     # Remove Public Key from Certificate Store
     if (Test-Path Cert:\LocalMachine\TrustedPeople\$PublicThumbprint) {
-        Write-Host "Removing WinRM Public Certificate from the certificate store..."
+        Write-Host "Removing WinRM Public Certificate from the 'TrustedPeople' certificate store..."
         Remove-Item Cert:\LocalMachine\TrustedPeople\$PublicThumbprint
     } Else {
-        Write-Host "WinRM Public Certificate is not installed. Skipping."
+        Write-Host "WinRM Public Certificate is not installed in the 'Trusted People' certificate store. Skipping."
     }
-
-
 }
 
+
 Function Main {
+    RemoveCodeSigningCertificates
     RemoveWinRMCertificates
 }
 
